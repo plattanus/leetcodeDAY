23ff012581abf558dd937c8b942f0dff28870cb9
root <root@lttwksp.lttwksp.mine>
2023-04-02 01:42:24 -0700

    23.4.2

--- 1039. 多边形三角剖分的最低得分.py
+++ 1039. 多边形三角剖分的最低得分.py
@@ -11,6 +11,7 @@ class Solution:
             if i + 2 == j:
                 return values[i] * values[i + 1] * values[j]
             return min((values[i] * values[k] * values[j] + dp(i, k) + dp(k, j)) for k in range(i + 1, j))
+        
         return dp(0, len(values) - 1)
 
     
